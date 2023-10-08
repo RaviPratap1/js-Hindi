409a97811e1bd9d1fcdec4ae2a17b8b67890eabc
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-08 08:10:43 +0000

    Conversion Of Data type

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "123"
+// console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber)  //String to number
+//console.log(valueInNumber)
+
+// input     output     type 
+//"33"        33        Number
+//"33abc"     NaN       Number
+//true        1         Number
+//false       0         Number
+
+let isLoggedIn = 1;
+let booleaanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(typeof booleaanIsLoggedIn)
+//console.log(booleaanIsLoggedIn)
+
+// input       output     type 
+//  1          true       boolean 
+//  0          false      boolean 
+//  ""         false      boolean 
+//  "Ravi"     true       boolean 
+
+let soneNumber= 33
+let stringNumber = String(soneNumber);
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+// input       output     type 
+// 33          33         string
