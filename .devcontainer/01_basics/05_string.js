ff37d672795baa8a4829b1374eeb93180d15ffba
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-08 11:56:19 +0000

    string done

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,14 @@
+const Name = "Ravi";
+// Or 
+const gameName= new String('hitesh')
+
+const repoCount = 50
+// console.log(Name+ repoCount+ "value")
+// shortCut 
+console.log(`Hello my name is ${Name} and my repo count is ${repoCount}`)
+
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('s'))
