ea747a6f0bb146d92fcb5a5be235ad3f92f00551
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-09 08:31:08 +0000

    date done

---
+++ .devcontainer/01_basics/07_dates.js
@@ -0,0 +1,31 @@
+let myDate = new Date()
+// console.log(myDate.toString());  //Mon Oct 09 2023 05:13:53 GMT+0000
+// console.log(myDate.toDateString());  //Mon Oct 09 2023
+// console.log(myDate.toLocaleString());  //10/9/2023, 5:15:23 AM
+// console.log(typeof myDate);                 //object
+
+let myCreatedDate = new Date(2023,0,23);
+// console.log(myCreatedDate);               //2023-01-23T00:00:00.000Z
+// console.log(myCreatedDate.toDateString());     //Mon Jan 23 2023
+// console.log(myCreatedDate.toLocaleString());     //1/23/2023, 12:00:00 AM     **************
+
+let myCreatedDate1 = new Date("01-14-2023");
+// console.log(myCreatedDate1.toLocaleString());   //console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStemp =Date.now() 
+// Convert millisecond +++++++++++
+// console.log(myTimeStemp)
+// console.log(myCreatedDate1.getTime())
+
+// console.log(Math.floor(myTimeStemp/1000))    // Convert cecond
+
+// console.log(myDate.getMonth())
+// console.log(myDate.getDay())
+// console.log(myDate.getFullYear())
+// console.log(myDate.getMilliseconds())
+
+console.log(myDate.toLocaleString('default', {
+    weekday:"long",
+    day:"2-digit"
+}))
