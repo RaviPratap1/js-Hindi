7f188ec2be58e00aba1a53aed8a87c181b6dfb92
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-09 04:55:18 +0000

    Math and Num

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,47 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+
+const numStr =balance.toString()   //number to String
+const numStr1 =score.toString()   //number to String
+// console.log(numStr)
+// console.log(typeof numStr)
+// console.log(typeof numStr1)
+
+// console.log(balance.toFixed(3))     //.000
+// console.log(99.042144.toFixed(2))     //.04 
+// console.log(99.042144.toFixed())     // 99 
+
+
+const otherNumber = 123.6966
+const otherNumber1 = 123.1966
+const otherNumber2 = 123.5
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber1.toPrecision(3))
+// console.log(otherNumber2.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//+++++++++++++++++++++Maths++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+//console.log(Math.random())   //0> Math.random() <1
+//console.log(Math.floor(Math.random()*10)+1)   //0< Math.random() <=10
+const min =10;
+const max =20;
+//console.log(Math.floor(Math.random()*(max-min+1)+min))  //10<= Math.random() <=20
+
+
+
+
