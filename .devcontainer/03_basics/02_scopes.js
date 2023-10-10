9b55bf8a111e12a35ea4162c1a4a61e4bd2f08b2
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-10 05:57:51 +0000

    scope done

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -10,12 +10,44 @@ if(true){
     let a= 40;
     const b = 50;
     // var c = 60; 
-    console.log(a)
+    // console.log(a)
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one () {
+const username = "Ravi"
+
+function two () {
+    const website = "youtube"
+    console.log(username)
+    
+    }
+    // console.log(website)  //not valid
+    two()
+}
+// one()
+
+// function two method ++++++++++++++++
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num+1;
+}
+
+// console.log(addTwo(8))      ---> ERROR 
+const addTwo = function (num){
+    return num+2;
+}
+
+console.log(addOne(5))
+console.log(addTwo(8))
+
+
 
 
 
