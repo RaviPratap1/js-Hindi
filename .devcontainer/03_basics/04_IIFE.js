c3f8810693dffbd6a8f6309dc39a845ee60c37b5
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-10 07:07:50 +0000

    IIFE Done

---
+++ .devcontainer/03_basics/04_IIFE.js
@@ -0,0 +1,23 @@
+// Immediately Incoked function Expressionss (IIFE)
+
+// normal +++++++++++++++++++++
+function chai(){
+    console.log("DB Connected")
+}
+// chai()
+
+
+// (IIFE)++++++++++++++++++
+(function chai(){
+    // named IIFE 
+    console.log("DB Connected")
+})();
+
+( () => {
+    console.log("DB Connected-1 ")
+})();
+
+( (name) => {
+    console.log(`DB Connected-2 ${name}`)
+})('ravi');
+
