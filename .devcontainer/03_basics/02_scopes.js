a8c419b65364607baa12e85f5fb039abce27675e
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-10 05:03:16 +0000

    scopes Part 1 Done

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+let a= 10;
+// const b = 20;
+// var c = 30;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+if(true){
+    let a= 40;
+    const b = 50;
+    // var c = 60; 
+    console.log(a)
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+
+
