c1dbc1e06fcf9e9539cfe8de3f116b851d274893
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-10-11 09:26:08 +0000

    if

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,43 @@
+// if 
+ 
+// if(condition){
+
+// }
+
+// short hand 
+
+const balance = 200;
+
+// if(balance<500) console.log("test");
+
+// Falsy values 
+//false, 0, -0,  bigInt 0n, "", null, undefined, NaN
+
+// truthy values 
+// "0", 'false', " ", [], {}, function() {}
+
+const  userEmail = []
+if(userEmail.length ===0){
+    // console.log("userEmail array is empty")
+}
+
+const  emptyObj = {}
+if(Object.keys(emptyObj).length ===0){
+    // console.log("Object is empty")
+}
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+val1 = undefined ?? 15
+
+console.log(val1)
+
+
+// Terniary Oprator 
+const ice =100;
+ice >90 ? console.log("take"):console.log("don't take")
+ice >110 ? console.log("take"):console.log("don't take")
