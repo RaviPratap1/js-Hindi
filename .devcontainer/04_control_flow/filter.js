3919c7d9ea3362a321f80defca9c77ba47f0f699
Ravi Pratap <108719457+RaviPratap1@users.noreply.github.com>
2023-11-09 10:10:39 +0000

    new update

--- .devcontainer/04_control_flow/filter.js
+++ .devcontainer/04_control_flow/filter.js
@@ -10,15 +10,15 @@ const books = [
 ]
 
 const userBooks = books.filter( (bk) => bk.genre === "history")
-// console.log(userBooks)
+console.log(userBooks)
 
 
 const myNum = [1,2,3,4,5,6,7,8,9,10]
 
 // const newNum =myNum.map( (num) => num+10 )
 const newNum =myNum
-                    .map( (num) => num*10  )
+                    // .map( (num) => num*10  )
                     .map( (num) => num+1)
-                    .filter( (num) => num >= 40)   // true false return
+                    //.filter( (num) => num >= 40)   // true false return
  
 console.log(newNum)
